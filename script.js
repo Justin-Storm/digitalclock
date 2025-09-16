afba26875b9ef206b0a4a178b894bced696c15f1
Justin-Storm <144840724+Justin-Storm@users.noreply.github.com>
2025-09-16 13:10:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const timeString = document.querySelector('span');
 
 function updateTime() {
     const time = new Date();
-    const hour = time.getHours();
+    let hour = time.getHours();
     const minute = time.getMinutes();
     const second = time.getSeconds();
-    
-    timeString.innerHTML = `${hour}:${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second : second}${hour < 12 ? " AM" : " PM"}`;
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+
+    hour = hour % 12;
+    hour = hour ? hour : 12;
+
+    timeString.innerHTML = `${hour}:${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second : second} ${ampm}`;
 }
 
-setInterval(() => {
-    updateTime();
-});
+setInterval(updateTime, 1000);
